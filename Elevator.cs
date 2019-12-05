7656a99112105465cf79f9c2093204f20bcdaa81
Leonid <add.object@bk.ru>
2019-12-06 01:05:04 +0300

    init

---
+++ Elevator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ElevatorModeling
+{
+    public class Elevator
+    {
+        private int capacity;
+        private int floors;
+        private int currentFloor = 1;
+
+        public List<Person> elevatorParty = new List<Person>();
+
+        public Elevator(int capacity, int floors)
+        {
+            this.capacity = capacity;
+            this.floors = floors;
+        }
+
+        public void GoUp()
+        {
+            if (currentFloor < floors)
+            {
+                Thread.Sleep(1000);
+                currentFloor += 1;
+            }
+        }
+
+        public void GoDown()
+        {
+            if (currentFloor > 1)
+            {
+                Thread.Sleep(1000);
+                currentFloor -= 1;
+            }
+        }
+        public void Dislodge()
+        {
+            foreach (var person in elevatorParty)
+            {
+                if (person.ArrivalFLoor == currentFloor)
+                {
+                    Thread.Sleep(1000);
+                    elevatorParty.Remove(person);
+                }
+            }
+        }
+
+        public void AddPerson(Person person)
+        {
+            if (elevatorParty.Count < capacity)
+            {
+                Thread.Sleep(1000);
+                elevatorParty.Add(person);
+            }
+        }
+    }
+    
+}
---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElevatorModeling
+{
+    public class Person
+    {
+        private int _departureFloor;
+        private int _arrivalFloor;
+
+        public int DepartureFloor => _departureFloor;
+        public int ArrivalFLoor => _arrivalFloor;
+
+        public Person(int departureFloor, int arrivalFloor)
+        {
+            _departureFloor = departureFloor;
+            _arrivalFloor = arrivalFloor;
+        }
+
+    }
+}
